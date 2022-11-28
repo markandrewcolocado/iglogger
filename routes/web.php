46ae1168afd03a8a194859088f4cfbc2758a3420
Mark Andrew Colocado <mark05@simplexi.com>
2022-11-28 16:46:22 +0800

    Added routes.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
+
+Route::get('/event', [EventController::class, 'verifyRequest']);
+Route::post('event', [EventController::class, 'receiveEvent']);
